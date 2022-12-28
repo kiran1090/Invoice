c3236fcb337e17a6ff8c572bb56f2a141b3501a6
kiran_a <iamkiranashok@gmail.com>
2022-12-28 17:15:45 +0530

    Second commit

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Invoice {
+public class Invoice {
+    public String ID, Desc;
+    public Double UnitPrice, TotalPrice;
+    int Quantity;
+
+    public Invoice(String ID, String Desc, Double UnitPrice, int Quantity) {
+        this.ID = ID;
+        this.Desc = Desc;
+        this.UnitPrice = UnitPrice;
+        this.Quantity = Quantity;
+    }
+
+    public Double getTotalPrice() {
+        return TotalPrice = UnitPrice * Quantity;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "ID='" + ID + '\'' +
+                ", Desc='" + Desc + '\'' +
+                ", UnitPrice=" + UnitPrice +
+                ", Quantity=" + Quantity +
+                '}';
+    }
 }
+
--- src/TestInvoice.java
+++ src/TestInvoice.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class TestInvoice {
+public class TestInvoice {
+    public static void main(String [] args)
+    {
+        Invoice Iv1= new Invoice("a1","pen",20.00,2);
+        Iv1.getTotalPrice();
+        System.out.println(Iv1);
+        System.out.println("id is "+Iv1.ID);
+        System.out.println("Total Price" +Iv1.TotalPrice);
+    }
 }
